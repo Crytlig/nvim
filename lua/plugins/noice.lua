850537179b5506594ee967d2c41e8ff2fb2c77be
Claes Rytlig <crytlig@hotmail.com>
2025-04-29 22:34:49 +0200

    Add noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,50 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    presets = {
+      bottom_search = false,
+      command_palette = false, -- Disable command_palette preset for custom positioning
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = false,
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "40%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = "48%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    },
+  },
+}
